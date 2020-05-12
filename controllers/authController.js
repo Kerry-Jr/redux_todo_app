0de867ad1f345f2de695f74850a4aae625e8004e
Kerry-Jr <kerrysfs@gmail.com>
2020-05-12 11:54:28 -0700

    added user auth

---
+++ controllers/authController.js
@@ -0,0 +1,8 @@
+const { isEmail, isLength } = require('validator');
+const { User } = require('./../models');
+
+module.exports = {
+    signUp: async (req, res) => {
+
+    }
+}
---
+++ routes/apiRoutes/authRoutes/index.js
@@ -0,0 +1,17 @@
+// declare all of our routes for signing a user in or signing a user up
+
+const router = require('express').Router();
+
+
+
+router.post('/signup');
+
+
+
+
+
+
+
+
+
+module.exports = router;

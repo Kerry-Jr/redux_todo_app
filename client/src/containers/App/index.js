d7aec3b89da1e7a5ff065fa7bc6e1a801c9f10e7
Kerry-Jr <kerrysfs@gmail.com>
2020-05-15 13:57:50 -0700

    reduxform semantic ui email validation

--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 import { Grid } from 'semantic-ui-react'
 import Counter from './../Counter';
 import AllTodosList from '../AllTodosList';
+import SignUp from '../SignUp';
 import Navbar from './../../components/Navbar';
 class App extends Component {
     render() {
@@ -10,6 +11,7 @@ class App extends Component {
             <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
                 <Grid.Column style={{ maxWidth: 700 }}>
                     <Navbar/>
+                    <Route exact path='/' component={SignUp}/>
                     <Route exact path='/alltodos' component={AllTodosList}/>
                     <Route exact path='/counter' component={Counter}/>
                 </Grid.Column>

722a81ab0c3519dd6536edb00664b8cb2723ceeb
Kerry-Jr <kerrysfs@gmail.com>
2020-05-18 14:05:36 -0700

    fixed signout

--- client/src/containers/SignIn/index.js
+++ client/src/containers/SignIn/index.js
@@ -4,6 +4,9 @@ import { Form, Segment, Button } from 'semantic-ui-react';
 import { email, required } from 'redux-form-validators';
 import axios from 'axios';
 import { AUTH_USER } from '../../actions/types';
+
+
+
 class SignIn extends Component {
   // When the user submits the form, send the formValues to /api/auth/signin
   onSubmit = async (formValues, dispatch) => {
@@ -25,65 +28,65 @@ class SignIn extends Component {
   // Redirect the user to the '/counter' route
   renderEmail = ({ input, meta }) => {
     return (
-        <Form.Input
-            {...input}
-            fluid
-            error={ meta.touched && meta.error }
-            icon='user'
-            iconPosition='left'
-            autoComplete='off'
-            placeholder='Email address'
-        />
+      <Form.Input
+        {...input}
+        fluid
+        error={ meta.touched && meta.error }
+        icon='user'
+        iconPosition='left'
+        autoComplete='off'
+        placeholder='Email address'
+      />
     )
   }
   renderPassword = ({ input, meta }) => {
     return (
-        <Form.Input
-            {...input}
-            type='password'
-            fluid
-            error={ meta.touched && meta.error }
-            icon='lock'
-            iconPosition='left'
-            autoComplete='off'
-            placeholder='password'
-        />
+      <Form.Input
+        {...input}
+        type='password'
+        fluid
+        error={ meta.touched && meta.error }
+        icon='lock'
+        iconPosition='left'
+        autoComplete='off'
+        placeholder='password'
+      />
     )
   }
   render() {
     const { handleSubmit, invalid, submitting, submitFailed } = this.props;
     return (
-        <Form size='large' onSubmit={handleSubmit(this.onSubmit)}>
-          <Segment stacked>
-            <Field
-                name='email'
-                component={this.renderEmail}
-                validate={
-                  [
-                    required({ msg: 'Email is required' }),
-                    email({ msg: 'You must provide a valid email address' })
-                  ]
-                }
-            />
-            <Field
-                name='password'
-                component={this.renderPassword}
-                validate={
-                  [
-                    required({ msg: 'You must provide a password' })
-                  ]
-                }
-            />
-            <Button
-                content='Sign In'
-                color='teal'
-                fluid
-                size='large'
-                type='submit'
-                disabled={ invalid || submitting || submitFailed }
-            />
-          </Segment>
-        </Form>
+      <Form size='large' onSubmit={handleSubmit(this.onSubmit)}>
+        <Segment stacked>
+          <Field
+            name='email'
+            component={this.renderEmail}
+            validate={
+              [
+                required({ msg: 'Email is required' }),
+                email({ msg: 'You must provide a valid email address' })
+              ]
+            }
+          />
+          <Field
+            name='password'
+            component={this.renderPassword}
+            validate={
+              [
+                required({ msg: 'You must provide a password' })
+              ]
+            }
+          />
+          <Button
+            content='Sign In'
+            color='teal'
+            fluid
+            size='large'
+            type='submit'
+            disabled={ invalid || submitting || submitFailed }
+          />
+        </Segment>
+      </Form>
     )
   }
 }
--- client/src/containers/SignOut/index.js
+++ client/src/containers/SignOut/index.js
@@ -2,23 +2,27 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { Container, Message, Button } from 'semantic-ui-react';
-import { signOut} from '../../actions/auth';
 
-class Signout extends Component {
+import { signOut } from '../../actions/auth';
 
-    componentDidMount() {
-        this.props.signOut();
-    }
+class SignOut extends Component {
 
-    render() {
-        return (
-            <Container>
-                <Message error header="We're sorry to see you go :(" />
-                <Button as={Link} to='/' content='Go back to sign up' />
 
-            </Container>
-        );
-    }
+  componentDidMount() {
+    console.log("Did mount");
+    this.props.signOut();
+  }
+
+  render() {
+    console.log("render");
+    return (
+      <Container>
+        <Message error header="We're sorry to see you go :("/>
+
+        <Button as={Link} to='/' content='Go back to sign up'/>
+      </Container>
+    );
+  }
 }
 
-export default connect(null, { signOut })(Signout);
+export default connect(null, { signOut })(SignOut);
